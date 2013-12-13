84cd446228cd70e8cf071acc2d4e939e61934b65
Roman Pavlyuk <roman@z-index.com.ua>
2013-12-13 17:40:42 +0200

    + tests

--- hw5 (jquery)/app.js
+++ hw5 (jquery)/app.js
@@ -14,6 +14,12 @@
 //
 //$red.width('100') //изменяет текущую высоту до 100px всех DOM элементов с классом .red
 
+//Написать реализацию CSS геттеро-сеттера в jQuery которая может принимать на вход ключ значение или объект
+//со значениями. Для стилизации текущего селектора: цвет, dimensions и других параметров( стоит
+//огранничиться 5 параметрами, так как их слишком много ). Сделать возможным, чтобы эти методы были
+//chainable. Добавить возможность, передавать второй /третий аргумент как время, чтобы стили
+//применились через какое-то время.
+
 function my$(selector){
     if(!(this instanceof my$)){
         return new my$(selector);
@@ -86,24 +92,34 @@ function my$(selector){
 }
 
 var $div = my$('div')
-console.log( $div ) //показать все div на странице.
+//console.log( $div ) //показать все div на странице.
 
 var $red = my$('.red')
-console.log( $red ) //показать все DOM элементы с классом .red, которые присутсвуют на странице
+//console.log( $red ) //показать все DOM элементы с классом .red, которые присутсвуют на странице
 
 $red.width('100').height(120); //изменяет текущую высоту до 100px всех DOM элементов с классом .red
 
-//Написать реализацию CSS геттеро-сеттера в jQuery которая может принимать на вход ключ значение или объект
-//со значениями. Для стилизации текущего селектора: цвет, dimensions и других параметров( стоит
-//огранничиться 5 параметрами, так как их слишком много ). Сделать возможным, чтобы эти методы были
-//chainable. Добавить возможность, передавать второй /третий аргумент как время, чтобы стили
-//применились через какое-то время.
+my$('span').css('color', '#000').css({
+    'font-size':'12px',
+    'height': '30px'
+});
 
-my$('span').css('color', '#000').css({'font-size':'12px','height': '30px'});
+test("my$, target .red and style change", function() {
+    var div = document.getElementsByClassName('red'),
+        span = document.getElementsByTagName('span');
+
+    equal(my$('.red')[0], div[0], 'Selected target is ok');
+    equal(div[0].style.width, '100px', 'width was set to 100px');
+    equal(div[0].style.height, '120px', 'height was set to 120px');
+
+    equal(span[0].style.color, 'rgb(0, 0, 0)', 'color was set to #000');
+    equal(span[0].style.fontSize, '12px', 'font-size was set to 12px');
+    equal(span[0].style.height, '30px', 'height was set to 30px');
+});
 
 //Добавить delegate метод к вашему wrapped set'у из задания https://gist.github.com/dmitryt/7590375
 
-// TODO: add delegate method after TableTraverse complete
+// TODO: add delegate method after TableTraverse will be completed
 
 //Бонусные очки: добавить возможность, передавать второй /третий аргумент как время, чтобы стили применились через какое-то время
 
@@ -112,4 +128,18 @@ my$('span').css({
     'height': '20px',
     'border-color':'red'
 }, 1500);
+
+test("my$ style change after delay", function() {
+    stop(); // Pause the test
+    //Add your wait
+    setTimeout(function() {
+        var span = document.getElementsByTagName('span');
+
+        ok(true, 'delay has passed');
+        equal(span[0].style.fontSize, '10px', 'font-size was set to 10px');
+        equal(span[0].style.borderColor, 'red', 'border-color was set to red');
+        equal(span[0].style.height, '20px', 'height was set to 20px');
+        start();
+    }, 1500);
+});
 
