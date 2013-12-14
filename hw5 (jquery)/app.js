25b69b9f7f813f3697865413b9ce71c6b1c9e4d7
Yaneraz <raz.pavlyuk@gmail.com>
2013-12-14 20:19:31 +0200

    hw5 reworked

--- hw5 (jquery)/app.js
+++ hw5 (jquery)/app.js
@@ -27,21 +27,25 @@ function my$(selector){
 my$.fn = my$.prototype = {
     constructor: my$,
     init: function(selector){
-        var nodesArray = Array.prototype.slice.call(document.querySelectorAll(selector));
+        var nodeList = Array.prototype.slice.call(document.querySelectorAll(selector));
 
-        if ( selector != null ) {
-            Array.prototype.push.apply( this, nodesArray );
+        for (var i = 0; i < nodeList.length; i++){
+            this[i] = nodeList[i];
         }
+
+        this.length = nodeList.length;
 
         return this;
     },
     // our custom each engine :)
     each: function(fn){
-        this.forEach(function(el){
+        for (var i = 0; i < this.length; i++){
             if (typeof fn === 'function'){
-                fn.apply(el, arguments)
+                (function(el){
+                    fn.apply(el, arguments);
+                })(this[i]);
             }
-        });
+        }
 
         return this;
     },
@@ -86,25 +90,6 @@ my$.fn = my$.prototype = {
         }
 
         return this;
-    },
-    merge: function( first, second ) {
-        var l = second.length,
-            i = first.length,
-            j = 0;
-
-        if ( typeof l === "number" ) {
-            for ( ; j < l; j++ ) {
-                first[ i++ ] = second[ j ];
-            }
-        } else {
-            while ( second[j] !== undefined ) {
-                first[ i++ ] = second[ j++ ];
-            }
-        }
-
-        first.length = i;
-
-        return first;
     }
 };
 my$.fn.init.prototype = my$.fn;
@@ -117,26 +102,26 @@ var $div = my$('div')
 //console.log( $div ) //показать все div на странице.
 
 var $red = my$('.red')
-console.log( $red ) //показать все DOM элементы с классом .red, которые присутсвуют на странице
+//console.log( $red ) //показать все DOM элементы с классом .red, которые присутсвуют на странице
 
 $red.width('100').height(120); //изменяет текущую высоту до 100px всех DOM элементов с классом .red
 
-//my$('span').css('color', '#000').css({
-//    'font-size':'12px',
-//    'height': '30px'
-//});
+my$('span').css('color', '#000').css({
+    'font-size':'12px',
+    'height': '30px'
+});
 
 test("my$, target .red and style change", function() {
     var div = document.getElementsByClassName('red'),
         span = document.getElementsByTagName('span');
 
     equal(my$('.red')[0], div[0], 'Selected target is ok');
-//    equal(div[0].style.width, '100px', 'width was set to 100px');
-//    equal(div[0].style.height, '120px', 'height was set to 120px');
-//
-//    equal(span[0].style.color, 'rgb(0, 0, 0)', 'color was set to #000');
-//    equal(span[0].style.fontSize, '12px', 'font-size was set to 12px');
-//    equal(span[0].style.height, '30px', 'height was set to 30px');
+    equal(div[0].style.width, '100px', 'width was set to 100px');
+    equal(div[0].style.height, '120px', 'height was set to 120px');
+
+    equal(span[0].style.color, 'rgb(0, 0, 0)', 'color was set to #000');
+    equal(span[0].style.fontSize, '12px', 'font-size was set to 12px');
+    equal(span[0].style.height, '30px', 'height was set to 30px');
 });
 
 //Добавить delegate метод к вашему wrapped set'у из задания https://gist.github.com/dmitryt/7590375
@@ -145,23 +130,23 @@ test("my$, target .red and style change", function() {
 
 //Бонусные очки: добавить возможность, передавать второй /третий аргумент как время, чтобы стили применились через какое-то время
 
-//my$('span').css({
-//    'font-size':'10px',
-//    'height': '20px',
-//    'border-color':'red'
-//}, 1500);
-//
-//test("my$ style change after delay", function() {
-//    stop(); // Pause the test
-//    //Add your wait
-//    setTimeout(function() {
-//        var span = document.getElementsByTagName('span');
-//
-//        ok(true, 'delay has passed');
-//        equal(span[0].style.fontSize, '10px', 'font-size was set to 10px');
-//        equal(span[0].style.borderColor, 'red', 'border-color was set to red');
-//        equal(span[0].style.height, '20px', 'height was set to 20px');
-//        start();
-//    }, 1500);
-//});
+my$('span').css({
+    'font-size':'10px',
+    'height': '20px',
+    'border-color':'red'
+}, 1500);
+
+test("my$ style change after delay", function() {
+    stop(); // Pause the test
+    //Add your wait
+    setTimeout(function() {
+        var span = document.getElementsByTagName('span');
+
+        ok(true, 'delay has passed');
+        equal(span[0].style.fontSize, '10px', 'font-size was set to 10px');
+        equal(span[0].style.borderColor, 'red', 'border-color was set to red');
+        equal(span[0].style.height, '20px', 'height was set to 20px');
+        start();
+    }, 1500);
+});
 
