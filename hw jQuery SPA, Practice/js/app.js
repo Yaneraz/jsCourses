77668ee9adbd5c2550d9bdb43eb6b506eec2761e
Roman Pavlyuk <roman@z-index.com.ua>
2013-12-20 17:44:54 +0200

    hw SPA in progress

--- hw jQuery SPA, Practice/js/app.js
+++ hw jQuery SPA, Practice/js/app.js
@@ -1,37 +1,71 @@
 var myApp = {
     init: function(){
-        myApp.userList.getData();
+        myApp.login.render();
+        myApp.userList.render();
     }
 };
 
 myApp.userList = {
     elem: $('#list'),
-    data: {},
+    getTemplate: function(){
+        return $.get( "partials/user-list.html");
+    },
     render: function(){
-        var data = this.data;
-        var template =
-            '<ul class="small-block-grid-3">\
-                {{ users }}\
-                <li>\
-                    <div class="person {{ user.gender }}">\
-                        <a class="{{  }}" href="/user/{{ id }}"><i>{{ user.name.title }}</i> {{ user.name.last }}, {{ user.name.first }}</a>\
-                    </div>\
-                </li>\
-                {{/users }}\
-            </ul>';
+        var self = this;
 
-        this.elem.html(Mark.up(template, data));
+        $.when( this.getTemplate(), this.getData() ).done(function( a1, a2 ) {
+            // a1 and a2 are arguments resolved for the page1 and page2 ajax requests, respectively.
+            // Each argument is an array with the following structure: [ data, statusText, jqXHR ]
+            var template = a1[ 0 ],
+                data = a2[ 0 ];
+
+            self.elem.html(Mark.up(template, data));
+        });
     },
     getData: function(){
-        var self = myApp.userList;
-
-        $.get( "http://www.jscacourse.co.vu/users", function( data ) {
-            self.data.users = data;
-            self.render();
-        });
+        return $.get( "http://www.jscacourse.co.vu/users");
     }
 };
 
+myApp.login = {
+    elem: $('#login'),
+    getTemplate: function(){
+        return $.get( "partials/login.html");
+    },
+    render: function(){
+        var self = this;
+
+        $.when( this.getTemplate() ).done(function( a1 ) {
+            // a1 and a2 are arguments resolved for the page1 and page2 ajax requests, respectively.
+            // Each argument is an array with the following structure: [ data, statusText, jqXHR ]
+            var template = a1;
+
+            self.elem.html(Mark.up(template, null));
+            self.attachEvents();
+        });
+    },
+    attachEvents: function(){
+        $('input#submitButton').click( function() {
+            $.post( 'some-url', $('form#myForm').serialize(), function(data) {
+
+                },
+                'json' // I expect a JSON response
+            );
+        });
+
+        $('input#submitButton').click( function() {
+            $.ajax({
+                url: 'some-url',
+                type: 'post',
+                dataType: 'json',
+                data: $('form#myForm').serialize(),
+                success: function(data) {
+
+                }
+            });
+        });
+    }
+};
 
 $(function(){
     myApp.init();
